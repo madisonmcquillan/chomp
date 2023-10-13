1f4806214918fb754817c8c043bef789aaa3e02c
madisonmcquillan <m264212@usna.edu>
2023-10-13 09:45:23 -0400

    added sand

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ screen.fill((52, 222, 235))
 
 pygame.draw.rect(screen, (246, 230, 178), (0, 300, 400, 400))
 pygame.draw.rect(screen, (0, 200, 0), (200, 150, 75, 25))
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(sand, (200, 200, 64, 64))
 pygame.display.flip()
 while True:
     for event in pygame.event.get():
