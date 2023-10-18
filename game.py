c6a60c1a4e755b7bbec4e3a8fdfb0db8be0369fe
madisonmcquillan <m264212@usna.edu>
2023-10-18 09:26:44 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,25 @@
 import pygame
 import sys
 
+# DIMENSIONS
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = 20
+TILE_SIZE = 64  # TILES ARE SQUARE HEIGHT == WIDTH
+
+# COLORS
+WATER_COLOR = (52, 222, 235)
+SAND_COLOR = (246, 230, 178)
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
-screen.fill((52, 222, 235))
+screen.fill(WATER_COLOR)
 
-pygame.draw.rect(screen, (246, 230, 178), (0, 300, 400, 400))
-pygame.draw.rect(screen, (0, 200, 0), (200, 150, 75, 25))
+pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT - SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (200, 200, 64, 64))
+screen.blit(sand, (SCREEN_WIDTH/2 - TILE_SIZE/2, SCREEN_HEIGHT/2 - TILE_SIZE/2))
+
 pygame.display.flip()
 while True:
     for event in pygame.event.get():
