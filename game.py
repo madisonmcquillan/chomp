ddae46866e51cec94ebb814667c9ba7b8a4c7771
madisonmcquillan <m264212@usna.edu>
2023-10-18 09:45:55 -0400

    added sand

--- game.py
+++ game.py
@@ -2,10 +2,10 @@ import pygame
 import sys
 
 # DIMENSIONS
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 400
-SAND_HEIGHT = 20
 TILE_SIZE = 64  # TILES ARE SQUARE HEIGHT == WIDTH
+SCREEN_WIDTH = 12*TILE_SIZE
+SCREEN_HEIGHT = 8*TILE_SIZE
+SAND_HEIGHT = 20
 
 # COLORS
 WATER_COLOR = (52, 222, 235)
@@ -18,7 +18,13 @@ screen.fill(WATER_COLOR)
 
 pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT - SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (SCREEN_WIDTH/2 - TILE_SIZE/2, SCREEN_HEIGHT/2 - TILE_SIZE/2))
+sand_top = pygame.image.load("assets/images/sand_top.png").convert()
+sand_top.set_colorkey((0, 0, 0))
+
+# BLIT SAND TILES ACROSS THE BOTTOM OF THE SCREEN
+for i in range(SCREEN_WIDTH // TILE_SIZE):
+    screen.blit(sand, (i*TILE_SIZE, SCREEN_HEIGHT-TILE_SIZE))
+    screen.blit(sand_top, (i*TILE_SIZE, SCREEN_HEIGHT-2*TILE_SIZE))
 
 pygame.display.flip()
 while True:
